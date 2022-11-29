b7177e3b2d1400598d131339ff4d9234ade525f3
Margarita Gibaduina <patokinam@yandex.ru>
2022-11-29 17:06:39 +0300

    add task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int number = ReadInt("Введите число: ");
+int sum = 0;
+
+while (number != 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе равна {sum}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
